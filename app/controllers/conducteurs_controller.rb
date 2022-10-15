ec65ea62081d7aa60b6ef814c822125d6b3cca43
Amine Lamnaouar <aminlamnaouar@gmail.com>
2022-10-15 13:29:50 +0200

    controller

--- app/controllers/conducteurs_controller.rb
+++ app/controllers/conducteurs_controller.rb
@@ -1,2 +1,17 @@
 class ConducteursController < ApplicationController
+
+  def new
+    @conducteur = Conducteur.new
+  end
+
+  def create
+  @conducteur = Conducteur.new(conducteur_params)
+  @conducteur.users_id = current_user.id
+  @conducteur.save!
+  redirect_to offres_path
+
+  def edit
+  end
+  
+end
 end
